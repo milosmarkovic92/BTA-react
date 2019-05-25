cb0d89b6891b2d016c9ee59159b6553cb11ce677
Milos Markovic <milosmarkovic7692@gmail.com>
2019-05-25 13:20:21 +0200

    Updates

---
+++ precache-manifest.e6a9a686060f9f43cbdd51e080dd1b01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50c4789b4dc5fb81edf5af7b54307461",
+    "url": "/BTA-react/index.html"
+  },
+  {
+    "revision": "fa4ccc1227a806ded178",
+    "url": "/BTA-react/static/css/main.4c6a4d39.chunk.css"
+  },
+  {
+    "revision": "927b4c7ac0bcccf587e8",
+    "url": "/BTA-react/static/js/2.21825336.chunk.js"
+  },
+  {
+    "revision": "fa4ccc1227a806ded178",
+    "url": "/BTA-react/static/js/main.7b106f16.chunk.js"
+  },
+  {
+    "revision": "5427641836b87bd8f3c3",
+    "url": "/BTA-react/static/js/runtime~main.aeee02d2.js"
+  }
+]);
