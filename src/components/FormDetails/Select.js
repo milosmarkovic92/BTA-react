ad8ffe5483ffa2ebf061b75cf29a5be228c87b39
Milos Markovic <milosmarkovic@software-nation.com>
2019-05-28 16:45:44 +0200

    initial commit

--- src/components/FormDetails/Select.js
+++ src/components/FormDetails/Select.js
@@ -4,10 +4,10 @@ import * as constants from '../constants/constants';
 const professions = constants.professions;
 
 export default function Selects(props) {
-  const { name, defaultValues, onChange } = props;
+  const { name, defaultValues, onChange, label } = props;
   return (
     <React.Fragment>
-      {/* <label htmlFor={name}>{label}</label> */}
+      <label htmlFor={name}>{label}</label>
       <select name={name} value={defaultValues} onChange={onChange} id="">
         {professions.map((profession, index) => {
           if (profession === professions[0]) {
