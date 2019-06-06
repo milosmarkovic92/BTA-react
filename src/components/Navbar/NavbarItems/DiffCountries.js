e1f6a00c95defb71e6a7865e1e99f1cab046943d
Milos Markovic <milosmarkovic@software-nation.com>
2019-06-06 16:29:11 +0200

    added functionality to checkboxes

--- src/components/Navbar/NavbarItems/DiffCountries.js
+++ src/components/Navbar/NavbarItems/DiffCountries.js
@@ -7,7 +7,9 @@ export default class DiffCountries extends Component {
     this.state = {
       isLoading: true,
       trenutniUser: [],
-      contacts: []
+      contacts: [],
+      phoneChecked: [],
+      websiteChecked: []
     };
   }
 
@@ -23,9 +25,40 @@ export default class DiffCountries extends Component {
       .then(parsedJSON => {
         this.setState({ trenutniUser: parsedJSON });
       })
-
       .catch(error => console.log("error is: ", error));
   };
+
+  checkPhoneItem = (e) => {
+    const {checked} = e.target;
+    fetch("https://jsonplaceholder.typicode.com/users/" + e.target.value)
+    .then(response => response.json())
+    .then(parsedJSON => 
+      
+      {
+      checked ?
+      this.setState({ phoneChecked: parsedJSON }) :
+      this.setState({phoneChecked: []})
+    }
+    )
+
+    .catch(error => console.log("error is: ", error));
+  }
+
+  checkWebsiteItem = (e) => {
+    const {checked} = e.target;
+    fetch("https://jsonplaceholder.typicode.com/users/" + e.target.value)
+    .then(response => response.json())
+    .then(parsedJSON => 
+      
+      {
+      checked ?
+      this.setState({ websiteChecked: parsedJSON }) :
+      this.setState({websiteChecked: []})
+    }
+    )
+
+    .catch(error => console.log("error is: ", error));
+  }
 
   fetchData() {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -35,7 +68,9 @@ export default class DiffCountries extends Component {
           id: `${user.id}`,
           name: `${user.name}`,
           username: `${user.username}`,
-          email: `${user.email}`
+          email: `${user.email}`,
+          phone: `${user.phone}`,
+          website: `${user.website}`
         }))
       )
       .then(contacts =>
@@ -52,15 +87,17 @@ export default class DiffCountries extends Component {
     return (
       <div className="container">
         <div className="row">
-          <div className="filter-container col-6 mt-5">
+          <div className="filter-container col-8 mt-5">
             <div className="filters">
               {/* ovde bi selekti trebali da se razbiju u komponente */}
-              <select
-                className="mb-4"
-                name=""
-                id=""
-                onChange={this.onChangeHandler}
-              >
+              
+              <select 
+                className="select" 
+                defaultValue="user" 
+                onChange={this.onChangeHandler}>
+                <option disabled value="user">
+                  Select user
+                </option>
                 {contacts.map(contact => {
                   const { email, name, id } = contact;
                   return (
@@ -70,31 +107,60 @@ export default class DiffCountries extends Component {
                   );
                 })}
               </select>
-              <select className="mb-4">
+              <select className="select">
                 {this.state.trenutniUser !== undefined ? (
                   <option value="">{this.state.trenutniUser.email}</option>
                 ) : null}
               </select>
-              <select>
+              <select className="select">
                 {this.state.trenutniUser !== undefined ? (
                   <option value="">{this.state.trenutniUser.username}</option>
                 ) : null}
               </select>
-              <div className="mt-2">
-                <input type="checkbox" />
-                <input type="checkbox" />
-                <input type="checkbox" />
-                <input type="checkbox" />
-                <input type="checkbox" />
+              <div className="select">
+                <input className="checkbox" type="checkbox" 
+                  value={this.state.trenutniUser.id}
+                  onClick={this.checkPhoneItem}
+                  />
+                <label className="mr-3" htmlFor="">
+                  Local Transport
+                </label>
+                <input className="checkbox" type="checkbox" 
+                  value={this.state.trenutniUser.id}
+                  onClick={this.checkWebsiteItem}/>
+                <label className="mr-3" htmlFor="">
+                  Entertainment
+                </label>
+                <input className="checkbox" type="checkbox" />
+                <label className="mr-3" htmlFor="">
+                  Safety
+                </label>
+                <input className="checkbox" type="checkbox" />
+                <label className="mr-3" htmlFor="">
+                  Local Culture
+                </label>
+                <input className="checkbox" type="checkbox" />
+                <label htmlFor="">Airport</label>
               </div>
             </div>
           </div>
-          <div className="filter-results col-6 mt-5">
+          <div className="filter-results col-4 mt-5">
             {this.state.trenutniUser !== undefined ? (
               <div>
                 <h4>{this.state.trenutniUser.name}</h4>
                 <p>{this.state.trenutniUser.email}</p>
                 <p>{this.state.trenutniUser.username}</p>
+                {
+                  this.state.phoneChecked !== undefined ?
+                  <p>{this.state.phoneChecked.phone}</p> :
+                    null
+
+                }
+                {
+                  this.state.websiteChecked !== undefined ?
+                  <p>{this.state.websiteChecked.website}</p> :
+                    null
+                }
               </div>
             ) : null}
           </div>
